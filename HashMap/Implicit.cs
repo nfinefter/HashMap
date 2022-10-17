0f54d483c26d5c1e2a94342e5d4b4e3102a3741d
User <User@NIKA>
2022-10-17 09:34:26 -0700

    Add project files.

---
+++ HashMap/Implicit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashMap
+{
+    public interface Implicit
+    {
+        public string Id { get;}
+        public int Age { get;}
+    }
+}
