2bf693c64e7ffd680535b262d72941071f9f8379
Nicholas Finefter <nfinefter@gmail.com>
2022-10-19 19:02:27 -0700

    Added add, remove, clear, indexer, and GetNode Function

--- HashMap/HashMap.cs
+++ HashMap/HashMap.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,20 +11,31 @@ using System.Threading.Tasks;
 namespace HashMap
 {
     public class HashMap<TKey, TValue> : IDictionary<TKey, TValue>
-    {
+    { 
         public LinkedList<KeyValuePair<TKey, TValue>>[] items;
 
+#nullable disable
         public TValue this[TKey key]
         {
             get
-            {
+            { 
+                LinkedListNode<KeyValuePair<TKey, TValue>> node = GetNode(key);
 
+                if (node == null) throw new KeyNotFoundException();
+                       
+                return node.Value.Value;                
             }
             set
             {
 
+                LinkedListNode<KeyValuePair<TKey, TValue>> node = GetNode(key);
+
+                if (node == null) throw new KeyNotFoundException();
+
+                node.Value = KeyValuePair.Create(key, node.Value.Value); 
             }
         }
+#nullable enable
         public ICollection<TKey> Keys => throw new NotImplementedException();
 
         public ICollection<TValue> Values => throw new NotImplementedException();
@@ -32,28 +44,40 @@ namespace HashMap
 
         public bool IsReadOnly => throw new NotImplementedException();
 
-        
+
 
         public void Add(TKey key, TValue value)
         {
-            var index = (key.GetHashCode()) % items.Length;
-
-            if (items[index] == null)
-            {
-                LinkedList<KeyValuePair<TKey, TValue>> keyValuePairs = new LinkedList<KeyValuePair<TKey, TValue>>();
-                keyValuePairs.AddLast();
-                //Add the KeyValuePair
-            }
+            Add(KeyValuePair.Create(key, value));
         }
 
         public void Add(KeyValuePair<TKey, TValue> item)
         {
+            var index = (item.Key.GetHashCode()) % items.Length;
 
+            if (items[index] == null)
+            {
+                items[index] = new LinkedList<KeyValuePair<TKey, TValue>>();
+
+                items[index].AddLast(item);
+            }
+            else
+            {
+                LinkedListNode<KeyValuePair<TKey, TValue>> node = GetNode(item.Key);
+
+                if (item.Key.Equals(node.Value.Key))
+                {
+                    throw new Exception("Duplicate key");
+                }
+
+
+                items[index].AddLast(item);
+            }
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            items = null;
         }
 
         public bool Contains(KeyValuePair<TKey, TValue> item)
@@ -78,12 +102,23 @@ namespace HashMap
 
         public bool Remove(TKey key)
         {
-            throw new NotImplementedException();
+            LinkedListNode<KeyValuePair<TKey, TValue>> node = GetNode(key);
+
+            if (node == null) return false;
+
+            node.List.Remove(node);
+            return true;
         }
 
         public bool Remove(KeyValuePair<TKey, TValue> item)
         {
-            throw new NotImplementedException();
+
+            LinkedListNode<KeyValuePair<TKey, TValue>> node = GetNode(item.Key);
+
+            if (node == null) return false;
+
+            node.List.Remove(node);
+            return true;
         }
 
         public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
@@ -95,6 +130,38 @@ namespace HashMap
         {
             throw new NotImplementedException();
         }
+
+        public LinkedListNode<KeyValuePair<TKey, TValue>> GetNode(TKey key)
+        {
+
+            var index = (key.GetHashCode()) % items.Length;
+
+            if (items[index] == null) return null;
+
+            for (var temp = items[index].First; temp != null; temp = temp.Next)
+            {
+                if (key.Equals(temp.Value.Key)) return temp;
+            }
+            
+            return null;
+
+        }
+        public LinkedListNode<KeyValuePair<TKey, TValue>> GetNode(KeyValuePair<TKey, TValue> item)
+        {
+
+            var index = (item.Key.GetHashCode()) % items.Length;
+
+            if (items[index] == null) return null;
+
+            for (var temp = items[index].First; temp != null; temp = temp.Next)
+            {
+                if (item.Key.Equals(temp.Value.Key)) return temp;
+            }
+            return null;
+
+
+        }
+
 
     }
 }
